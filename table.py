85f844676cc208fa11e89ffcec7d2eebd6da5b22
Janek <janekpriv@gmail.com>
2025-01-07 20:12:42 +0100

    merging disabled temporarly so that it works with fb

--- table.py
+++ table.py
@@ -5,31 +5,39 @@ import pandas as pd
 def createTable(phones_list, filename):
 
     path = os.path.join("phones_csv", filename)
-
+    print(path)
     if os.path.exists(path):
         #load existing csv file
         existing_df = pd.read_csv(path)
     else:
         #create empty table
-        existing_df = pd.DataFrame(
-            columns=[
-                "title",
-                "price",
-                "id"
-                "link",
-                "time_location"
-            ]
-        )
+        if "fb" in filename:
+            existing_df = pd.DataFrame(
+                columns=["title","price","link"]
+            )
+        else:
+            existing_df = pd.DataFrame(
+                columns=["title","price","id","link","time_location"]
+            )
 
-    data = {
-        "title" : [phone['title'] for phone in phones_list ],
-        "price" : [phone['price']for phone in phones_list],
-        "id" : [phone['id']for phone in phones_list],
-        "link" : [phone['link'] for phone in phones_list],
-        "time_location" : [phone['time_location'] for phone in phones_list],
-    }
+
+    if "fb" in filename:
+        data ={
+            "title" : [phone['title'] for phone in phones_list ],
+            "price" : [phone['price']for phone in phones_list],
+            "link" : [phone['link'] for phone in phones_list]
+        }
+    else:
+        data = {
+            "title" : [phone['title'] for phone in phones_list ],
+            "price" : [phone['price']for phone in phones_list],
+            "id" : [phone['id']for phone in phones_list],
+            "link" : [phone['link'] for phone in phones_list],
+            "time_location" : [phone['time_location'] for phone in phones_list ] 
+        }
     #create table with scraped info
     new_df = pd.DataFrame(data)
+    print(new_df)
 
     #add scrap date to all elements in the table
     new_df['scrape_date'] = pd.Timestamp.now().date()
@@ -37,14 +45,18 @@ def createTable(phones_list, filename):
 
     #merge and remove duplicates 
     merged_df = pd.concat([existing_df, new_df], ignore_index=True)
-    merged_df = merged_df.drop_duplicates(subset='id', keep='first')
+    if "fb" in filename:
+       # merged_df = merged_df.drop_duplicates(subset='link', keep='first')
+       pass
+    else :
+        merged_df = merged_df.drop_duplicates(subset='id', keep='first')
     
     #reset index
-    merged_df=merged_df.reset_index(drop=True)
+    #merged_df=merged_df.reset_index(drop=True)
 
     #remove NAN price values -> problems with plotting later
-    merged_df['price'] = pd.to_numeric(merged_df['price'], errors='coerce')
-    merged_df = merged_df.dropna(subset=['price'])
+    #merged_df['price'] = pd.to_numeric(merged_df['price'], errors='coerce')
+    #merged_df = merged_df.dropna(subset=['price'])
 
     #save to file 
     merged_df.to_csv(path, index=False)
