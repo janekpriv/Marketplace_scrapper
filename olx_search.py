147db1d9b0e4bf54d58ec42cd1f49a7e3aa53866
Janek <janekpriv@gmail.com>
2025-01-08 18:13:51 +0100

    fixed bugs

--- olx_search.py
+++ olx_search.py
@@ -20,7 +20,8 @@ class olxSearcher:
         prices = soup.find_all(string=re.compile(r"zł$"))
         self.phonelist = []
         for item in prices:
-
+            if item.parent.parent.find('a') is None:
+                continue
             link = f"https://www.olx.pl/{item.parent.parent.find('a').get('href')}/"
             title = item.parent.parent.find("h4").string
             price_element =item.parent.parent.parent.find_all("p")[0]
@@ -36,10 +37,13 @@ class olxSearcher:
             if price is not None:
                 price = re.sub("[zł ]","",price).strip()
                 price_float = float(re.sub("[,]",".",price).strip())
-
+            else:
+                price_float = None
             time_location = item.parent.parent.parent.find_all("p")[1].string
             
-            if price_float>=self.MINPRICE and price_float<=self.MAXPRICE:
+            
+
+            if price_float is not None and price_float>=self.MINPRICE and price_float<=self.MAXPRICE:
 
                 if self.PHONE_NAME in title.lower() or re.sub("[ ]","",self.PHONE_NAME) in title.lower():
                     
@@ -52,43 +56,43 @@ class olxSearcher:
                         "id" : id # for database purpouses
                     }
                     self.phonelist.append(phone)
-        response = requests.get(self.OLX_URL_PAGE2) 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        prices = soup.find_all(string=re.compile(r"zł$"))
+        # response = requests.get(self.OLX_URL_PAGE2) 
+        # soup = BeautifulSoup(response.text, 'html.parser')
+        # prices = soup.find_all(string=re.compile(r"zł$"))
 
-        for item in prices:
+        # for item in prices:
 
-            link = f"https://www.olx.pl/{item.parent.parent.find('a').get('href')}/"
-            title = item.parent.parent.find("h4").string
-            price_element =item.parent.parent.parent.find_all("p")[0]
-            id = item.find_parent('div', {'data-cy': 'l-card'}).get('id')
+        #     link = f"https://www.olx.pl/{item.parent.parent.find('a').get('href')}/"
+        #     title = item.parent.parent.find("h4").string
+        #     price_element =item.parent.parent.parent.find_all("p")[0]
+        #     id = item.find_parent('div', {'data-cy': 'l-card'}).get('id')
 
             
-            for span in price_element.find_all("span"):
-                span.decompose()
+        #     for span in price_element.find_all("span"):
+        #         span.decompose()
 
-            price = price_element.string
+        #     price = price_element.string
 
 
-            if price is not None:
-                price = re.sub("[zł ]","",price).strip()
-                price_float = float(re.sub("[,]",".",price).strip())
+        #     if price is not None:
+        #         price = re.sub("[zł ]","",price).strip()
+        #         price_float = float(re.sub("[,]",".",price).strip())
 
-            time_location = item.parent.parent.parent.find_all("p")[1].string
+        #     time_location = item.parent.parent.parent.find_all("p")[1].string
             
-            if price_float>=self.MINPRICE and price_float<=self.MAXPRICE:
+        #     if price_float>=self.MINPRICE and price_float<=self.MAXPRICE:
 
-                if self.PHONE_NAME in title.lower() or re.sub("[ ]","",self.PHONE_NAME) in title.lower():
+        #         if self.PHONE_NAME in title.lower() or re.sub("[ ]","",self.PHONE_NAME) in title.lower():
                     
 
-                    phone = {
-                        "link": link,
-                        "title" : title,
-                        "price" : price,
-                        "time_location" : time_location,
-                        "id" : id # for database purpouses
-                    }
-                    self.phonelist.append(phone)
+        #             phone = {
+        #                 "link": link,
+        #                 "title" : title,
+        #                 "price" : price,
+        #                 "time_location" : time_location,
+        #                 "id" : id # for database purpouses
+        #             }
+        #             self.phonelist.append(phone)
      
 
         return self.phonelist
