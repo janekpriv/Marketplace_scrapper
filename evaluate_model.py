741050317c7ead93c3bfead01ba9499bba35e052
Janek <janekpriv@gmail.com>
2025-01-09 20:04:38 +0100

    added todo, intialized file

---
+++ evaluate_model.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import re
+import os
+
+
+def evaluate_model(filename):
+
+    #TODO
+    #loads file from phones_csv
+    #selects title column 
+    #extracts model number from filename using re 
+    #goes through every row and checks what memory if any is specified in file
+    #checks whether or not phone is a pro model or not
+    #chceck if filename+model and storage csv file exists -> if not create one and write to it 
+    #remove that row from original filename 
+
+    #TO REMEBER 
+    #need to change filenames in send_message, or not?????
+
+    #call in main 
+
+
+
+
+
+    pass
+
+
