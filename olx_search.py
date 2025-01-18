98ed013901f931d99bc8d98bc53a3c0652fc3203
Janek <janekpriv@gmail.com>
2025-01-18 23:41:19 +0100

    added second page

--- olx_search.py
+++ olx_search.py
@@ -15,6 +15,19 @@ class olxSearcher:
     
 
     def getPhonesinfo(self):
+
+        BannedKeywords = [
+            "opakowanie",
+            "wymiana",
+            "plecki",
+            "korpus",
+            "naprawa",
+            "pokrowiec"
+        ]
+
+
+
+
         response = requests.get(self.OLX_URL_PAGE1) 
         soup = BeautifulSoup(response.text, 'html.parser')
         prices = soup.find_all(string=re.compile(r"zł$"))
@@ -45,6 +58,43 @@ class olxSearcher:
 
             if price_float is not None and price_float>=self.MINPRICE and price_float<=self.MAXPRICE:
 
+                if self.PHONE_NAME in title.lower() or re.sub("[ ]","",self.PHONE_NAME) in title.lower() and title.lower() not in BannedKeywords:
+                    
+
+                    phone = {
+                        "link": link,
+                        "title" : title,
+                        "price" : price,
+                        "time_location" : time_location,
+                        "id" : id # for database purpouses
+                    }
+                    self.phonelist.append(phone)
+        response = requests.get(self.OLX_URL_PAGE2) 
+        soup = BeautifulSoup(response.text, 'html.parser')
+        prices = soup.find_all(string=re.compile(r"zł$"))
+
+        for item in prices:
+
+            link = f"https://www.olx.pl/{item.parent.parent.find('a').get('href')}/"
+            title = item.parent.parent.find("h4").string
+            price_element =item.parent.parent.parent.find_all("p")[0]
+            id = item.find_parent('div', {'data-cy': 'l-card'}).get('id')
+
+            
+            for span in price_element.find_all("span"):
+                span.decompose()
+
+            price = price_element.string
+
+
+            if price is not None:
+                price = re.sub("[zł ]","",price).strip()
+                price_float = float(re.sub("[,]",".",price).strip())
+
+            time_location = item.parent.parent.parent.find_all("p")[1].string
+            
+            if price_float>=self.MINPRICE and price_float<=self.MAXPRICE:
+
                 if self.PHONE_NAME in title.lower() or re.sub("[ ]","",self.PHONE_NAME) in title.lower():
                     
 
@@ -56,43 +106,7 @@ class olxSearcher:
                         "id" : id # for database purpouses
                     }
                     self.phonelist.append(phone)
-        # response = requests.get(self.OLX_URL_PAGE2) 
-        # soup = BeautifulSoup(response.text, 'html.parser')
-        # prices = soup.find_all(string=re.compile(r"zł$"))
-
-        # for item in prices:
-
-        #     link = f"https://www.olx.pl/{item.parent.parent.find('a').get('href')}/"
-        #     title = item.parent.parent.find("h4").string
-        #     price_element =item.parent.parent.parent.find_all("p")[0]
-        #     id = item.find_parent('div', {'data-cy': 'l-card'}).get('id')
-
-            
-        #     for span in price_element.find_all("span"):
-        #         span.decompose()
-
-        #     price = price_element.string
-
-
-        #     if price is not None:
-        #         price = re.sub("[zł ]","",price).strip()
-        #         price_float = float(re.sub("[,]",".",price).strip())
-
-        #     time_location = item.parent.parent.parent.find_all("p")[1].string
-            
-        #     if price_float>=self.MINPRICE and price_float<=self.MAXPRICE:
-
-        #         if self.PHONE_NAME in title.lower() or re.sub("[ ]","",self.PHONE_NAME) in title.lower():
-                    
-
-        #             phone = {
-        #                 "link": link,
-        #                 "title" : title,
-        #                 "price" : price,
-        #                 "time_location" : time_location,
-        #                 "id" : id # for database purpouses
-        #             }
-        #             self.phonelist.append(phone)
+                    print(phone)
      
 
         return self.phonelist
